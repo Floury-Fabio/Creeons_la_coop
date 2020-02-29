911e4ecb9a40ec42067af201e935d0967f97d989
Floury-Fabio <floury-fabio@hotmail.fr>
2020-02-29 21:23:56 +0100

    apply suggestion for pull request 189

--- spec/jobs/send_subscription_end_alert_job_spec.rb
+++ spec/jobs/send_subscription_end_alert_job_spec.rb
@@ -2,48 +2,48 @@
 
 require 'rails_helper'
 
-RSpec.describe SendNotificationJob, type: :job do
+RSpec.describe SendSubscriptionEndAlertJob, type: :job do
   subject { ActionMailer::Base.deliveries }
 
   before do
     ActiveJob::Base.queue_adapter = :test
   end
 
-  context "when members are in end subscription period" do
-    it "sends an email to all of them" do
+  context "when some members' subscription are ending soon" do
+    it "sends an end of subscription alert mail to them" do
       end_subscription_period_params = { from: Date.today, to: Date.today + 15 }
       create_list :member, 5, end_subscription: Faker::Date.between(end_subscription_period_params)
       member_mails = Member.all.map(&:email)
-      SendNotificationJob.perform_now
+      SendSubscriptionEndAlertJob.perform_now
 
       expect(subject.map { |m| m.to[0] }).to match_array(member_mails)
     end
   end
 
-  context "when members are not in end subscription period" do
-    it "does not sends an email" do
+  context "when members' subscription are NOT ending soon" do
+    it "does not send them an end of subscription alert mail" do
       before_end_subscription_period_params = { from: Date.today + 16, to: Date.today + 300 }
       create_list :member, 5, end_subscription: Faker::Date.between(before_end_subscription_period_params)
-      SendNotificationJob.perform_now
+      SendSubscriptionEndAlertJob.perform_now
 
       expect(subject.size).to eq(0)
     end
   end
 
-  context "when members have an outdated subscription date" do
-    it "does not sends an email" do
-      before_end_subscription_period_params = { from: Date.today - 1, to: Date.today - 300 }
+  context "when members' end_subscription are outdated" do
+    it "does not send them an end of subscription alert mail" do
+      after_end_subscription_period_params = { from: Date.today - 1, to: Date.today - 300 }
       create_list :member, 5, end_subscription: Faker::Date.between(before_end_subscription_period_params)
-      SendNotificationJob.perform_now
+      SendSubscriptionEndAlertJob.perform_now
 
       expect(subject.size).to eq(0)
     end
   end
 
-  context "when members have a nil subscription date" do
-    it "does not sends an email" do
+  context "when members have a nil end_subscription date" do
+    it "does not send them an end of subscription alert mail" do
       create_list :member, 5, end_subscription: nil
-      SendNotificationJob.perform_now
+      SendSubscriptionEndAlertJob.perform_now
 
       expect(subject.size).to eq(0)
     end
